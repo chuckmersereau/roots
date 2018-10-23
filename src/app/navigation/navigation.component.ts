a80b61098d420e7a107c9f4fa6a2f5d0a664752c
chuck.mersereau <chuck.mersereau@MacBook-C02X70Q4JHD5.local>
2018-10-23 16:48:54 -0400

    navbar style edits

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from "@angular/common";
 
+const LINKS: any = [
+  { name: "Home", view: "/home", selected: false },
+  { name: "Prayer Requests", view: '/prayerrequests', selected: false },
+  { name: "Office", view: "/office", selected: false},
+  { name: "Field Ministry", view: '/fieldministry', selected: false },
+  { name: "Prayer Letters", view: '/prayerletters', selected: false },
+]
+
 @Component({
   selector: 'nav-bar',
   templateUrl: './navigation.component.html',
@@ -14,10 +22,7 @@ export class NavigationComponent implements OnInit {
     private location: Location
   ) {
 
-    this.links= [
-      { name: "Home", view: "/home", selected: false },
-      { name: "Field Ministry", view: '/fieldministry', selected: false }
-    ];
+    this.links = LINKS;
     this.location.go(this.location.path());
     this.changeSelected(this.location.path());
    }
--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { OfficeComponent } from './office/office.component';
 import { FieldMinistryComponent } from './field-ministry/field-ministry.component';
+import { PrayerLettersComponent } from './prayer-letters/prayer-letters.component';
+import { PrayerRequestsComponent } from './prayer-requests/prayer-requests.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'fieldministry', component: FieldMinistryComponent }
+  { path: 'prayerrequests', component: PrayerRequestsComponent },
+  { path: 'office', component: OfficeComponent },
+  { path: 'fieldministry', component: FieldMinistryComponent },
+  { path: 'prayerletters', component: PrayerLettersComponent },
 ];
 
 @NgModule({
