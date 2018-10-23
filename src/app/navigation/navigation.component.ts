6d8ace42b702df20564b9887256cab6e71487474
chuck.mersereau <chuck.mersereau@MacBook-C02X70Q4JHD5.local>
2018-10-23 14:30:49 -0400

    Navigation bar function and css style

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'nav-bar',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
+
+
 export class NavigationComponent implements OnInit {
+  private links: any;
+  constructor(
+    private location: Location
+  ) {
 
-  constructor() { }
+    this.links= [
+      { name: "Home", view: "/home", selected: false },
+      { name: "Field Ministry", view: '/fieldministry', selected: false }
+    ];
+    this.location.go(this.location.path());
+    this.changeSelected(this.location.path());
+   }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  changeSelected(view: string): void {
+    for(let link of this.links) {
+      if( link.view == view ) {
+        link.selected = true;
+      } else {
+        link.selected = false;
+      }
+    }
   }
-
 }
