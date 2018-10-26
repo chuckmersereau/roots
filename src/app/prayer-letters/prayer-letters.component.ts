42ededa58b93d292418f6830ae30a9c58884878c
chuck.mersereau <chuck.mersereau@MacBook-C02X70Q4JHD5.local>
2018-10-26 12:17:24 -0400

    Scrollable letters menu

--- src/app/prayer-letters/prayer-letters.component.ts
+++ src/app/prayer-letters/prayer-letters.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./prayer-letters.component.scss']
 })
 export class PrayerLettersComponent implements OnInit {
-
-  constructor() { }
+  letters: any;
+  letter: any;
+  constructor() {
+    this.letters = [
+      { name: 'September 2018', view:'assets/September_2018.pdf' },
+      { name: 'August 2018', view: 'assets/August_2018.pdf' }
+    ]
+    this.letter = this.letters[0];
+   }
 
   ngOnInit() {
   }
 
+  showLetter(letter: any) {
+    this.letter=letter;
+  }
+
+  viewLetter(): string {
+    console.log(this.letter.view);
+    return this.letter.view;
+  }
 }
