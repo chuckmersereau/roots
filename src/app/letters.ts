208c2d147f8ae0385b1cb4bdadb33b5af2d05afe
chuck.mersereau <chuck.mersereau@MacBook-C02X70Q4JHD5.local>
2018-11-13 11:40:02 -0500

    added years and collapsible lists to letter list. imported icon stylesheet. adjust height of prayer request form to accomodate new questions

--- src/app/letters.ts
+++ src/app/letters.ts
@@ -1,20 +1,26 @@
 export const LETTERS = [
   {
-    name: 'October 2018',
+    year: 2018,
+    date: new Date('October 2018'),
+    selected: false,
     link: 'https://drive.google.com/open?id=1ZHwd9C3r-3pbjGxd3nCyUGNMqB8KwUoy',
     view: [
       'https://drive.google.com/uc?id=1ZB9N4IZsTvDUaGiswQ2H4V0hhqMMp_Za'
     ]
   },
   {
-    name: 'September 2018',
+    year: 2019,
+    date: new Date('September 2019'),
+    selected: false,
     link:"https://drive.google.com/open?id=1zhWn652yC78Cy-aEqAXfcyIEDlKYyrWe",
     view: [
       'https://drive.google.com/uc?id=1K-iBYkIR4jSoYs_tDAcTQ_HyjXaN_Rrn',
       'https://drive.google.com/uc?id=13QxX7GSPsmKSVYTBnc9JPew7HjEcJcZ5'
   ]},
   {
-    name: 'August 2018',
+    year: 2018,
+    date: new Date('August 2018'),
+    selected: false,
     link: "https://drive.google.com/open?id=10F3upGPyMPNFYePAsAw-1LV1DgeH604J",
     view: [
       'https://drive.google.com/uc?id=1PiZY3FkBabUcp0sTRV2VRWEJ_EEpOeqn',
--- src/app/prayer-letters/prayer-letters.component.ts
+++ src/app/prayer-letters/prayer-letters.component.ts
@@ -9,20 +9,60 @@ import { LETTERS } from '../letters';
 export class PrayerLettersComponent implements OnInit {
   letters: any;
   letter: any;
+  listYears: any;
+  years: any;
   constructor() {
+    this.years = [];
     this.letters = LETTERS;
-    this.letter = this.letters[0];
+    console.log(this.years);
+
+    this.letters.sort(function(a, b) {
+      return b.date - a.date;
+    });
+
+    this.getYears();
+    this.showLetter(this.letters[0]);
    }
 
   ngOnInit() {
   }
 
-  showLetter(letter: any) {
-    this.letter=letter;
+  showLetter(_letter: any) {
+    for(let letter of this.letters) {
+      if( letter.date == _letter.date) {
+        letter.selected = true;
+        this.letter=letter;
+      } else {
+        letter.selected = false;
+      }
+    }
+    for(let year of this.years){
+      if(year.year == _letter.year) {
+        year.selected = true;
+      } else {
+        year.selected = false;
+      }
+    }
   }
 
-  viewLetter(): string {
-    console.log(this.letter.view);
-    return this.letter.view;
+  getYears() {
+    let listYears = [];
+    for(let letter of this.letters) {
+      if(listYears.indexOf(letter.year) == -1) {
+        listYears.push(letter.year)
+      }
+    }
+    for(let year of listYears){
+      this.years.push({ year: year, show:false, selected: false });
+    }
   }
+
+  showYear(_year: any) {
+    for(let year of this.years){
+      if(year.year == _year.year) {
+        year.show = !year.show;
+      }
+    }
+  }
+
 }
